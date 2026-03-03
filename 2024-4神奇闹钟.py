8058e6409499c3dd395e8aee8bbe46288f3965d3
aiaod1009 <3449508132@qq.com>
2026-03-03 21:52:13 +0800

    4

---
+++ 2024-4神奇闹钟.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from datetime import datetime, timedelta
+# 请在此输入您的代码
+
+epoch = datetime(1970, 1, 1, 0, 0, 0)
+
+T = int(input())
+
+for _ in range(T):
+  line = sys.stdin.readline().strip()
+  parts = line.split()  # 拆成 ["2016-09-07", "18:24:33", "10"]
+  time_str = " ".join(parts[:2])  # 合并前两项为完整时间字符串
+  x = int(parts[2])
+
+  ctime = datetime.strptime(time_str,"%Y-%m-%d %H:%M:%S")
+
+  total_s = (ctime - epoch).total_seconds()
+
+  interval = x * 60
+
+  last = total_s - (total_s % interval)
+
+  last_time = epoch + timedelta(seconds=last)
+
+  print(last_time.strftime("%Y-%m-%d %H:%M:%S"))

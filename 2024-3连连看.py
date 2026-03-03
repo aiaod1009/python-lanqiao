999d809ddb1e878b82fe32937844cf55207428a6
aiaod1009 <3449508132@qq.com>
2026-03-03 21:36:41 +0800

    3

---
+++ 2024-3连连看.py
@@ -0,0 +1,31 @@
+import os
+import sys
+# 请在此输入您的代码
+n,m = map(int,input().split())
+arr = []
+for i in range(n):
+      row = list(map(int,input().split()))
+      arr.append(row)
+
+pos = {}
+for i in range(n):
+  for j in range(m):
+    val = arr[i][j]
+    if val not in pos:
+        pos[val] = []
+    pos[val].append((i, j))
+
+ans = 0
+
+# 遍历每个数字的所有位置对
+for val in pos:
+    lst = pos[val]
+    k = len(lst)
+    for i in range(k):
+        a, b = lst[i]
+        for j in range(i + 1, k):
+            c, d = lst[j]
+            if abs(a - c) == abs(b - d) > 0:
+                ans += 2
+
+print(ans)

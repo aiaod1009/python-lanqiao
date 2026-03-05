e8e8c1fc16572cc19a7ae9e47fef31dd72302f7e
aiaod1009 <3449508132@qq.com>
2026-03-05 17:33:00 +0800

    7

--- 2023-12023.py
+++ 2023-12023.py
@@ -1,21 +1,28 @@
-def solve(digit):
-    digit = str(digit)
-    count = 0
-    for j in digit:
-        if j == "2" and count == 0:
-            count = 1
-        elif j == "0" and count == 1:
-            count = 2
-        elif j == "2" and count == 2:
-            count = 3
-        elif j == "3" and count == 3:
-            count = 4
-    if count == 4:
-        return True
-
-
+import os
+import sys
 ans = 0
-for i in range(12345678, 98765433):
-    if solve(i):
+def find_i(i):
+#具体来说，它查找第一个'2'的位置，然后从该位置的下一个字符开始查找'0'的位置......
+    find_1 = i.find('2')
+    if find_1 == -1:
+        return 0
+
+    find_2 = i.find('0', find_1 + 1)
+    if find_2 == -1:
+        return 0
+
+    find_3 = i.find('2', find_2 + 1)
+    if find_3 == -1:
+        return 0
+
+    find_4 = i.find('3', find_3 + 1)
+    if find_4 == -1:
+        return 0
+
+    return 1
+
+for i in range(12345678, 98765432 + 1):
+    if find_i(str(i)) == 0:
         ans += 1
-print(98765432 - 12345678 - ans)
+print(ans)
+# print(85959030)

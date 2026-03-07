eac098c6b0aadc5456219fdd0cc65bb74e82ec76
aiaod1009 <3449508132@qq.com>
2026-03-07 16:38:14 +0800

    7

---
+++ 2022-4数位排序.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# 请在此输入您的代码
+
+n = int(input())
+m = int(input())
+nums = list(range(1, n+1))
+
+def sort_key(x):
+  s = sum(int(c) for c in str(x))
+  return (s,x)
+
+nums_s = sorted(nums, key=sort_key)
+print(nums_s[m-1])

0796fd680e30f63b6647e0dc985f3c669fe08238
aiaod1009 <3449508132@qq.com>
2026-03-07 19:45:22 +0800

    8

--- 2022-3纸张尺寸.py
+++ 2022-3纸张尺寸.py
@@ -2,17 +2,28 @@ import os
 import sys
 
 # 请在此输入您的代码
-arr =[[] for _ in range(9)]
-arr[0] = [1189,841]
-for i in range(1,9):
-  if arr[i-1][0] > arr[i-1][1]:
-    l = arr[i-1][0] //2
-    w = arr[i-1][1]
-  else:
-    w = arr[i-1][0]
-    l = arr[i-1][1] // 2
-  arr[i] = [l,w]
+# arr =[[] for _ in range(9)]
+# arr[0] = [1189,841]
+# for i in range(1,9):
+#   if arr[i-1][0] > arr[i-1][1]:
+#     l = arr[i-1][0] //2
+#     w = arr[i-1][1]
+#   else:
+#     w = arr[i-1][0]
+#     l = arr[i-1][1] // 2
+#   arr[i] = [w,l]
+#
+# s = int(input()[1:])
+# print(arr[s][0])
+# print(arr[s][1])
 
-s = int(input()[1:])
-print(arr[s][0])
-print(arr[s][1])
+
+l=1189
+w=841
+i=0
+s=input()
+n=int(s[-1])
+for i in range(n):
+  l,w=w,l//2
+print(l)
+print(w)

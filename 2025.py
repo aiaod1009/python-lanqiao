f085cead3e1346a008a953028150b834a1d07178
aiaod1009 <3449508132@qq.com>
2026-03-10 21:17:09 +0800

    4

--- 2025.py
+++ 2025.py
@@ -1,4 +1,4 @@
-#1
+#1模拟
 # n = 2025
 # s = 0
 # while n > 0:
@@ -22,19 +22,36 @@
 # print(s)
 
 
-#2
-with open("words.txt",'r',encoding="UTF-8") as f:
-    data = [l.strip() for l in f if l.strip()]
-data.sort(key=lambda x:len(x))
+#2字符串模拟+状态dp
+# with open("words.txt",'r',encoding="UTF-8") as f:
+#     data = [l.strip() for l in f if l.strip()]
+# data.sort(key=lambda x:len(x))
+#
+# bea = set()
+# ans = ''
+# for i in data:
+#     if len(i) == 1 or ''.join(sorted(i[:-1])) in bea:
+#         bea.add(''.join(sorted(i)))
+#         if len(i) > len(ans) or (len(i) == len(ans) and i < ans):
+#             ans = i
+# print(ans)
 
-bea = set()
-ans = ''
-for i in data:
-    if len(i) == 1 or ''.join(sorted(i[:-1])) in bea:
-        bea.add(''.join(sorted(i)))
-        if len(i) > len(ans) or (len(i) == len(ans) and i < ans):
-            ans = i
-print(ans)
+
+
+3模拟
+w,h,v = map(int,input().split())
+for i  in range(h + w):
+    if i < h:
+        print(''.join('Q' for _ in range(w)))
+    else:
+        print(''.join('Q' for _ in range(w+v)))
+
+for i in range(h):
+    print("Q" * w)
+for i in range(w):
+    print("Q" * (w+v))
+
+
 
 
 

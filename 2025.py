7d1506ee1a99a6473313ae60e34d81bc69d731c8
aiaod1009 <3449508132@qq.com>
2026-03-10 20:24:02 +0800

    3

--- 2025.py
+++ 2025.py
@@ -1,21 +1,58 @@
-n = 2025
-s = 0
-while n > 0:
-    if s % 2 == 0:
-        n -= (5+2)
-        if s % 3 == 1:
-            n -= 2
-        elif s % 3 == 2:
-            n -= 10
-        else:
-            n -= 7
-    else:
-        n -= (5+15)
-        if s % 3 == 1:
-            n -= 2
-        elif s % 3 == 2:
-            n -= 10
-        else:
-            n -= 7
-    s += 1
-print(s)
+#1
+# n = 2025
+# s = 0
+# while n > 0:
+#     if s % 2 == 0:
+#         n -= (5+2)
+#         if s % 3 == 1:
+#             n -= 2
+#         elif s % 3 == 2:
+#             n -= 10
+#         else:
+#             n -= 7
+#     else:
+#         n -= (5+15)
+#         if s % 3 == 1:
+#             n -= 2
+#         elif s % 3 == 2:
+#             n -= 10
+#         else:
+#             n -= 7
+#     s += 1
+# print(s)
+
+
+#2
+with open("words.txt",'r',encoding="UTF-8") as f:
+    data = [l.strip() for l in f if l.strip()]
+data.sort(key=lambda x:len(x))
+
+bea = set()
+ans = ''
+for i in data:
+    if len(i) == 1 or ''.join(sorted(i[:-1])) in bea:
+        bea.add(''.join(sorted(i)))
+        if len(i) > len(ans) or (len(i) == len(ans) and i < ans):
+            ans = i
+print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

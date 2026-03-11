2cc1fd9a1024fa7e58f190cd9b496b27339bf868
aiaod1009 <3449508132@qq.com>
2026-03-11 23:10:46 +0800

    4

---
+++ 2024.py
@@ -0,0 +1,17 @@
+# 1
+def si(n):
+    if n == 0:
+        return '0'
+    res = []
+    while n > 0:
+        res.append(str(n % 4))
+        n = n // 4
+    return ''.join(reversed(res))
+
+count = 0
+for i in range(1,2025):
+    sum1 = sum(int(c) for c in bin(i)[2:])
+    sum2 = sum(int(c) for c in si(i))
+    if sum1 == sum2:
+        count += 1
+print(count)

8989e5524252cc095c3f2a83d2f3cd2aac0aade2
aiaod1009 <3449508132@qq.com>
2026-03-12 13:10:08 +0800

    4

--- 2024.py
+++ 2024.py
@@ -23,29 +23,52 @@
 # y = pow(7,10000,mod) #又没7又没3的
 # print((n-2*x+y)%mod)
 
-#3
-from collections import defaultdict
-n,m = map(int,input().split())
-a=[]
-for i in range(n):
-    a.append(list(map(int, input().split())))
+#3 模拟，哈希表
+# from collections import defaultdict
+# n,m = map(int,input().split())
+# a=[]
+# for i in range(n):
+#     a.append(list(map(int, input().split())))
+#
+# diag1 = defaultdict(lambda: defaultdict(int))
+# diag2 = defaultdict(lambda: defaultdict(int))
+#
+# for i in range(n):
+#     for j in range(m):
+#         val = a[i][j]
+#         diag1[val][i-j] += 1
+#         diag2[val][i+j] += 1
+# count = 0
+# for val in diag1:
+#     for n in diag1[val].values():
+#         count += n * (n-1)
+#     for n in diag2[val].values():
+#         count += n * (n-1)
+#
+# print(count)
 
-diag1 = defaultdict(lambda: defaultdict(int))
-diag2 = defaultdict(lambda: defaultdict(int))
+# 4
+import sys
+from datetime import datetime,timedelta
+T = int(input())
 
-for i in range(n):
-    for j in range(m):
-        val = a[i][j]
-        diag1[val][i-j] += 1
-        diag2[val][i+j] += 1
-count = 0
-for val in diag1:
-    for n in diag1[val].values():
-        count += n * (n-1)
-    for n in diag2[val].values():
-        count += n * (n-1)
+d0 = datetime(1970,1,1,0,0,0)
 
-print(count)
+for _ in range(T):
+    s = sys.stdin.readline().strip()
+    part = s.split()
+    t = ' '.join(part[:2])
+    x = int(part[2])
+    t1 = datetime.strptime(t,"%Y-%m-%d %H:%M:%S")
+    total = (t1 - d0).total_seconds()
+    x *= 60
+    last = total - (total % x)
+
+    ans = d0 + timedelta(seconds=last)
+    print(ans.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+
 
 
 

e4a2afde39fbdb0d9fdee1bffca02b162f56cd68
aiaod1009 <3449508132@qq.com>
2026-03-13 23:45:36 +0800

    7

--- 2023.py
+++ 2023.py
@@ -1,20 +1,49 @@
-#1 2023  暴力枚举 f
-def find_i(i):
-    a = i.find('2')
-    if a == -1:
-        return 0
-    b = i.find('0',a+1)
-    if b == -1:
-        return 0
-    c = i.find('2', b + 1)
-    if c == -1:
-        return 0
-    d = i.find('3', c + 1)
-    if d == -1:
-        return 0
-    return 1
-cnt = 0
-for i in range(12345678,98765433):
-    if find_i(str(i)) == 0:
-        cnt += 1
-print(cnt)
+#1 暴力枚举
+# def find_i(i):
+#     a = i.find('2')
+#     if a == -1:
+#         return 0
+#     b = i.find('0',a+1)
+#     if b == -1:
+#         return 0
+#     c = i.find('2', b + 1)
+#     if c == -1:
+#         return 0
+#     d = i.find('3', c + 1)
+#     if d == -1:
+#         return 0
+#     return 1
+# cnt = 0
+# for i in range(12345678,98765433):
+#     if find_i(str(i)) == 0:
+#         cnt += 1
+# print(cnt)
+
+# 2 枚举
+max_cnt = [0] * (4046)
+for i in range (1,2024):
+    max_cnt[i] = i
+
+for i in range(1,2024):
+    for j in range(i + 1,2024):
+        s = i + j
+        add = min(i,j)
+        max_cnt[s] += add
+print(max(max_cnt))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

fe8de592e743c389ac35e944aa6283ee3d34e4ca
aiaod1009 <3449508132@qq.com>
2026-03-14 17:31:59 +0800

    5

--- 2023-3松散子序列.py
+++ 2023-3松散子序列.py
@@ -4,15 +4,16 @@ import sys
 # 请在此输入您的代码
 s = input().strip()
 n = len(s)
+def val(c):
+    return ord(c)-96
 if n == 0:
     print(0)
 elif n == 1:
-    print(ord(s[0]) - ord('a') + 1)
+    print(val(s[0]))
 else:
     dp = [0] * n
-    dp[0] = ord(s[0]) - ord('a') + 1
-    dp[1] = max(dp[0], ord(s[1]) - ord('a') + 1)
+    dp[0] = val(s[0])
+    dp[1] = max(dp[0], val(s[1]))
     for i in range(2, n):
-        val = ord(s[i]) - ord('a') + 1
-        dp[i] = max(dp[i-1], dp[i-2] + val)
+        dp[i] = max(dp[i-1], dp[i-2] + val(s[i]))
     print(dp[-1])
--- 2023.py
+++ 2023.py
@@ -20,20 +20,31 @@
 # print(cnt)
 
 # 2 枚举
-max_cnt = [0] * 4046
-for i in range(1,2023):
-    max_cnt[i] += i
+# max_cnt = [0] * 4046
+# for i in range(1,2023):
+#     max_cnt[i] += i
+#
+# for i in range(1,2023):
+#     for j in range(i+1,2023):
+#         s = i + j
+#         add = min(i,j)
+#         max_cnt[s] += add
+# print(max(max_cnt))
 
-for i in range(1,2023):
-    for j in range(i+1,2023):
-        s = i + j
-        add = min(i,j)
-        max_cnt[s] += add
-print(max(max_cnt))
-
-# 3
-
-
+# 3 线性二维dp
+import sys
+input = lambda: sys.stdin.readline().strip()
+s = input()
+n = len(s)
+dp = [[0]*2 for _ in range(n+1)]
+def val(c):
+    return ord(c)-96
+# dp[0][0]=0
+# dp[0][1]=val(s[0])
+for i in range(1,n+1):
+    dp[i][0] = max(dp[i-1][0],dp[i-1][1])
+    dp[i][1] = dp[i-1][0] + val(s[i-1])
+print(max(dp[n][0],dp[n][1]))
 
 
 

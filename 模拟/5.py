ebb2bb703696130d772941443bb11e831628e1f2
aiaod1009 <3449508132@qq.com>
2026-03-15 16:27:06 +0800

    7

--- 模拟/5.py
+++ 模拟/5.py
@@ -1,7 +1,21 @@
-w, h, v = map(int, sys.stdin.readline().split())
-# 请在此输入您的代码
-for i in range(h):
-    print('Q' * w)
+#只能过60%
+# n = int(input())
+# tot = 0
+# while n > 0:
+#     n -= sum(int(i) for i in str(n))
+#     tot += 1
+# print(tot)
 
-for i in range(w):
-    print('Q' * (v + w))
+#过100%
+s = int(input())
+answer = 0
+while s != 0:
+    answer += 1
+    remove = 0
+    #反转，用str那些转化的话效率很慢
+    n = s
+    while n != 0:
+        remove += n % 10
+        n //= 10
+    s -= remove
+print(answer)
---
+++ 模拟/6.py
@@ -0,0 +1,7 @@
+w, h, v = map(int, sys.stdin.readline().split())
+# 请在此输入您的代码
+for i in range(h):
+    print('Q' * w)
+
+for i in range(w):
+    print('Q' * (v + w))

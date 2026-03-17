7ac04863bba3c36c3d0c7e2f5369e2d1b02cec02
aiaod1009 <3449508132@qq.com>
2026-03-17 23:23:11 +0800

    6:

--- 模拟/22.py
+++ 模拟/22.py
@@ -1,10 +1,12 @@
-n = int(input())
 s = 8100178706957568
-
 chars = "0123456789ABCDEFGHIGKLMNOPQRSTUVWXYZ"
-res = []
-
-while n > 0:
-    res.append(chars[n % x])
-    n //= x
-    res = ''.join(res)
+for i in range(11,37):
+    n = s
+    res = []
+    while n > 0:
+        res.append(chars[n % i])
+        n //= i
+    x = ''.join(res)
+    if x.isdigit():
+        print(i)
+        break

f4641931077bf6d7fc2225217242f8cb2ba6c349
aiaod1009 <3449508132@qq.com>
2026-03-18 22:42:03 +0800

    12

---
+++ 省一题库/枚举/7.py
@@ -0,0 +1,17 @@
+n = 0
+for i in range(12345678,98765433):
+    s=str(i)
+    a = s.find('2')
+    if a == -1:
+        continue
+    b = s.find('0',a)
+    if b == -1:
+        continue
+    c = s.find('2',b)
+    if c == -1:
+        continue
+    d = s.find('3',c)
+    if d == -1:
+        continue
+    n += 1
+print(98765433-12345678-n)

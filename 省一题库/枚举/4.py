29c4ed7bfaea97998de4f2ce87ab17f21c9df0c1
aiaod1009 <3449508132@qq.com>
2026-03-18 22:02:50 +0800

    9

--- 省一题库/枚举/4.py
+++ 省一题库/枚举/4.py
@@ -4,3 +4,10 @@ for i in a:
     if int(str(i)[-1]) == 3:
         ans += 1
 print(ans)
+
+#数学法
+count = 0
+for i in range(1, 2026):
+    if i % 10 == 7:
+        count += 1
+print(count)  # 结果也是 202

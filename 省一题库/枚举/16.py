1fac83039f5a58fd4052b5432a15b4fe55807556
aiaod1009 <3449508132@qq.com>
2026-03-19 18:36:36 +0800

    9

---
+++ 省一题库/枚举/16.py
@@ -0,0 +1,12 @@
+cnt = 0
+for n in range(1,1000001):
+    for a in range(1,int(n**0.5)+1):
+        if n % a == 0:
+            b = n // a
+            sa,sb = str(a),str(b)
+            sab = sa + sb
+            sn = str(n)
+            if sorted(sab) == sorted(sn):
+                cnt += 1
+            break
+print(cnt)

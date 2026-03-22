e289de96e4ab45ab1002ceaeebc3fdd9e2cd053b
aiaod1009 <3449508132@qq.com>
2026-03-22 12:38:01 +0800

    2

---
+++ 省一题库/贪心/14.py
@@ -0,0 +1,15 @@
+s = input().strip()
+n = len(s)
+i,j =0, n - 1
+cnt = 0
+while i < j:
+    while i < j and s[i] != 'A':
+        i += 1
+    while i < j and s[j] != 'B':
+        j -= 1
+    if i < j and s[i] == 'A' and s[j] == 'B':
+        if s[j] == 'B':
+            cnt += 1
+            i += 1
+            j -= 1
+print(n-2*cnt)

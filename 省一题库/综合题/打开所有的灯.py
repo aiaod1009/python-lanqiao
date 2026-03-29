5a255ef45e0bb3d74ac4ca671c861c4ce2fe6a90
aiaod1009 <3449508132@qq.com>
2026-03-29 13:49:43 +0800

    1

--- 省一题库/综合题/打开所有的灯.py
+++ 省一题库/综合题/打开所有的灯.py
@@ -4,13 +4,13 @@ flip = [
     [0, 3, 4, 6], [1, 3, 4, 5, 7], [2, 4, 5, 8],
     [3, 6, 7], [4, 6, 7, 8], [5, 7, 8]
 ]
-def min_steps(initial):
+def min_steps(ini):
     target = "111111111"
-    if initial == target:
+    if ini == target:
         return 0
     visited = set()   # 记录已经访问过的状态，避免重复搜索
-    q = deque([(initial, 0)])  # 队列存 (当前状态, 当前步数)
-    visited.add(initial)    # 初始状态标记为已访问
+    q = deque([(ini, 0)])  # 队列存 (当前状态, 当前步数)
+    visited.add(ini)    # 初始状态标记为已访问
     while q:  # 队列不为空就继续搜
         s, step = q.popleft()    # 取出队首元素（最早加入的，步数最少）
         for i in range(9):

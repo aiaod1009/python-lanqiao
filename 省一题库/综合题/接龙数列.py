b54c6470373d209a8d26b25889d253b418ebabc7
aiaod1009 <3449508132@qq.com>
2026-03-31 22:34:27 +0800

    1

---
+++ 省一题库/综合题/接龙数列.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = input().split()
+dp = [0] * 10
+for s in a:
+    h = int(s[0])
+    t = int(s[-1])
+    # dp[t] = 原来就有的、以t结尾的最长长度
+    # dp[h] + 1 = 把当前数字接上去后，新的长度
+    dp[t] = max(dp[t],dp[h]+1)
+print(n-max(dp))

fc270f23ca7889b34dca95c98cd7d60a679669a2
aiaod1009 <3449508132@qq.com>
2026-04-01 19:18:16 +0800

    1

---
+++ 省一题库/综合题/小笨的区间和疑惑.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = list(map(int,input().split()))
+
+l = [0] * n
+l[0] = a[0]
+for i in range(1,n):
+    l[i] = max(a[i],l[i-1]+a[i])
+
+r = [0] * n
+r[-1] = a[-1]
+for i in range(n-2,-1,-1):
+    r[i] = max(a[i],r[i]+a[i])
+
+ans = [l[i] + r[i] -a[i] for i in range(n)]
+print(' '.join(map(str,ans)))

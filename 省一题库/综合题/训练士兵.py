9664c316050838ed419f330835c4ba82131e96a6
aiaod1009 <3449508132@qq.com>
2026-04-02 13:55:20 +0800

    3

---
+++ 省一题库/综合题/训练士兵.py
@@ -0,0 +1,20 @@
+import sys
+input=lambda:sys.stdin.readline().strip()
+n,s = map(int,input().split())
+nums = [[0,0]] * n
+p,c = [0] * n,[0] *n
+for i in range(n):
+    nums[i] = list(map(int,input().split()))
+nums.sort(key=lambda x:x[1])
+for i in range(n):
+    p[i],c[i] = nums[i][0],nums[i][1]
+res = cnt = 0
+tot = sum(p)
+for i in range(n):
+    if tot >= s:
+        res += (c[i] - cnt) * s
+        cnt = c[i]
+    else:
+        res += (c[i] - cnt) * p[i]
+    tot -= p[i]
+print(res)

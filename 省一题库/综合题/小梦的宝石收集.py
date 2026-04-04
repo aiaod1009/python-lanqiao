93579bba914aba6936c5e37714435aca868725fe
aiaod1009 <3449508132@qq.com>
2026-04-04 20:15:14 +0800

    4

--- 省一题库/综合题/小梦的宝石收集.py
+++ 省一题库/综合题/小梦的宝石收集.py
@@ -6,16 +6,18 @@ for _ in range(t):
     a = [0] + a
     ans = 10 ** 18
     for i in range(1, n + 1):
+        # i是左端点
         if i - 1 > k:
             break
         l, r = i, n
         while l < r:
             mid = (l + r) // 2
-            # 核心操作次数公式，和AC代码完全一致
+            # 计算保留[i, mid]区间需要的操作次数
             cost = (i-1) + (n-mid) + min(i-1, n-mid)
             if cost <= k:
-                r = mid
+                r = mid  # 可行，尝试更小的r（缩小极差）
             else:
-                l = mid + 1
+                l = mid + 1 # 不可行，需要更大的r
+            # 到最后l==r
         ans = min(ans, a[l] - a[i])
     print(ans)

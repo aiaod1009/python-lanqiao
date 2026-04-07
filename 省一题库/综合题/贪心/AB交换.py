5eec08e8d22aeceabd08cc187edf1217b1b276b3
aiaod1009 <3449508132@qq.com>
2026-04-07 13:06:31 +0800

    2

--- 省一题库/综合题/贪心/AB交换.py
+++ 省一题库/综合题/贪心/AB交换.py
@@ -12,3 +12,22 @@ for _ in range(t):
         if i % 2 == 0 and s[i] != 'B': b += 1
         if i % 2 == 1 and s[i] != 'A': b += 1
     print(min(a, b) // 2)
+
+
+import sys
+input = lambda:sys.stdin.readline().strip()
+t = int(input())
+for _ in range(t):
+    ans = 0
+    ans2 = 0
+    n = int(input())
+    s = input()
+    s1 = 'AB'*n
+    s2 = 'BA'*n
+    for i in range(n*2):
+        if s[i] != s1[i]:
+            ans += 1
+        if s[i] != s2[i]:
+            ans2 += 1
+    print(min(ans,ans2) // 2)
+

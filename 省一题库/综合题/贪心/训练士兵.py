a84002a234f6327352683baf4029a51ba2e5ab45
aiaod1009 <3449508132@qq.com>
2026-04-08 21:06:37 +0800

    3

--- 省一题库/综合题/贪心/训练士兵.py
+++ 省一题库/综合题/贪心/训练士兵.py
@@ -1,10 +1,11 @@
 import sys
 input=lambda:sys.stdin.readline().strip()
 n,s = map(int,input().split())
-nums = [[0,0]] * n
+# nums = [[0,0] for _ in range(n)]
+nums = [list(map(int, input().split())) for _ in range(n))]
 p,c = [0] * n,[0] *n
-for i in range(n):
-    nums[i] = list(map(int,input().split()))
+# for i in range(n):
+    # nums[i] = list(map(int,input().split()))
 nums.sort(key=lambda x:x[1]) # 按升序
 for i in range(n):
     p[i],c[i] = nums[i][0],nums[i][1]

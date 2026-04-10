51a36a3dbbdd1adcd4427b4abe69a72317c0c56f
aiaod1009 <3449508132@qq.com>
2026-04-10 17:58:51 +0800

    2

--- 真题/14届/2023-3松散子序列.py
+++ 真题/14届/2023-3松散子序列.py
@@ -17,3 +17,26 @@ else:
     for i in range(2, n):
         dp[i] = max(dp[i-1], dp[i-2] + val(s[i]))
     print(dp[-1])
+
+s = input().strip()
+n = len(s)
+max_sum = 0
+# 暴力DFS：枚举选/不选
+def dfs(pos, last, current):
+    global max_sum
+    # 走到最后，更新最大值
+    if pos == n:
+        max_sum = max(max_sum, current)
+        return
+
+    # 不选当前字符
+    dfs(pos + 1, last, current)
+
+    # 选当前字符（必须满足：和上一个位置差 >=2）
+    if last == -1 or pos - last >= 2:
+        val = ord(s[pos]) - ord('a') + 1
+        dfs(pos + 1, pos, current + val)
+
+
+dfs(0, -1, 0)
+print(max_sum)

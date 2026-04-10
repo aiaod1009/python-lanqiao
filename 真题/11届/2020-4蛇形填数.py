246f6c86c287b08d30199ccb914c6139cdbe9bc1
aiaod1009 <3449508132@qq.com>
2026-04-10 18:54:29 +0800

    7

--- 真题/11届/2020-4蛇形填数.py
+++ 真题/11届/2020-4蛇形填数.py
@@ -11,3 +11,14 @@ sum_before = (k - 1) * k // 2  # 38×39//2=741
 result = sum_before + (k - i_target + 1)  # 741 + (39-20+1)=761
 
 print(result)  # 直接输出761
+
+
+import os
+import sys
+
+# 数学题目啦，直接找规律，中间斜线上面的是我们目标的数字，1、5、13、25......
+#很明显是递加上(n-1)*4+上一个数字嘛
+sum=1                #这里是因为第一个数字是1呀
+for i in range(1,21):
+  sum=(i-1)*4+sum
+print(sum)

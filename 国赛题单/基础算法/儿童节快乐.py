5fdecdcd8500ed52b1c8cb7c31d7938655e48219
aiaod1009 <3449508132@qq.com>
2026-05-15 19:48:43 +0800

    3

--- 国赛题单/基础算法/儿童节快乐.py
+++ 国赛题单/基础算法/儿童节快乐.py
@@ -5,7 +5,7 @@ total = 0
 for d in range(1, math.isqrt(prod) + 1):
     if prod % d == 0:
         e = prod // d
-        if (d + e) % 2 == 0:
+        if (d + e) % 2 == 0:  #必须是偶数
             a = (d + e) // 2
             n = a * a - k
             total += n

fa0622e2426a2261349aa4cc597281271375b5b8
aiaod1009 <3449508132@qq.com>
2026-05-15 19:24:16 +0800

    2

---
+++ 国赛题单/基础算法/儿童节快乐.py
@@ -0,0 +1,12 @@
+import math
+k = 10120300500
+prod = 2 * k
+total = 0
+for d in range(1, math.isqrt(prod) + 1):
+    if prod % d == 0:
+        e = prod // d
+        if (d + e) % 2 == 0:
+            a = (d + e) // 2
+            n = a * a - k
+            total += n
+print(total)

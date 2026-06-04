ad92e4306846037c012ab42509e55e4a6f4f1a69
aiaod1009 <3449508132@qq.com>
2026-06-04 14:22:11 +0800

    5

--- 省一题库/综合题/前缀和/求区间和.py
+++ 省一题库/综合题/前缀和/求区间和.py
@@ -3,9 +3,9 @@ input = lambda:sys.stdin.readline().strip()
 n = int(input())
 a = [0]+list(map(int,input().split()))
 m = int(input())
-s = [0] * (n+1)
+pre = [0] * (n+1)
 for i in range(1,n+1):
-    s[i] = s[i-1] + a[i]
+    pre[i] = pre[i-1] + a[i]
 for _ in range(m):
     l,r = map(int,input().split())
-    print(s[r] - s[l-1])
+    print(pre[r] - pre[l-1])
